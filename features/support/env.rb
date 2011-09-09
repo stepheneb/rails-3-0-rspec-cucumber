51377a2f0cd10decef08282845b75d4d988924d0
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-09-09 17:04:15 -0400

    include RSpec::Rails::Mocks in Cucumber World
    Still get  missing method mock_model running
    
      bin/cucumber features/manage_pages.feature

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,9 @@ require 'cucumber/rails'
 
 require File.expand_path('../../../spec/spec_helper.rb', __FILE__)
 
+require 'rspec/rails'
+require 'rspec/mocks'
+
 Capybara.default_selector = :css
 
 ActionController::Base.allow_rescue = false
@@ -12,15 +15,4 @@ rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 
-Before do
- # To get RSpec stubs and mocks working.
- $rspec_mocks ||= RSpec::Mocks::Space.new
-end
-
-After do
-   begin
-     $rspec_mocks.verify_all
-   ensure
-     $rspec_mocks.reset_all
-   end
-end
+World(RSpec::Rails::Mocks)
