cd6d3406b19b5d092476faa3f47470e7b8bc1459
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-09-12 12:10:53 -0400

    include RSpec:Mocks:ExampleMethods into World
    Allows use of RSpecRails mock_model in a cucumber feature.

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,4 +15,4 @@ rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 
-World(RSpec::Rails::Mocks)
+World(RSpec::Rails::Mocks, Rspec::Mocks::ExampleMethods)
